5154a2453274b496cff619a8224594ca237a72e7
unholydeath785 <ivansonofcoul@gmail.com>
2018-05-21 23:53:29 -0700

    undefined

--- IntegralCalculator/Program.cs
+++ IntegralCalculator/Program.cs
@@ -8,7 +8,7 @@ namespace IntegralCalculator
     {
         public static void Main(string[] args) {
             Calculator calculator = new Calculator();
-            Function function = new Function("f(x)= arcsin(x)");
+            Function function = new Function("f(x)= e^x");
             Interval interval = new Interval(0, 1);
             double n = calculator.calculateDefiniteIntegral(function, interval);
             Console.WriteLine(n);
