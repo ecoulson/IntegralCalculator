dd2239eed3ad626b6517ae3b686818a55b3a92d7
unholydeath785 <ivansonofcoul@gmail.com>
2018-05-23 08:57:40 -0700

    updated file input

--- IntegralCalculator/Program.cs
+++ IntegralCalculator/Program.cs
@@ -19,7 +19,7 @@ namespace IntegralCalculator
                 double n = calculator.calculateDefiniteIntegral(function, interval);
                 string line = string.Format("function: {0,-40} interval: ({1,-16}, {2,-16}) result: {3,-16}\n", functionExpressions[i], interval.getStartPoint(), interval.getEndPoint(), n);
                 result += line;
-                Console.WriteLine((i + 1) + " / 30 integrals taken...");
+                Console.WriteLine((i + 1) + " / {0} integrals taken...", functionExpressions.Length);
             }
             File.WriteAllText(@"/Users/evancoulson/IntegralCalculator/functions_out.txt", result);
             //CalculatorClient client = new CalculatorClient();
